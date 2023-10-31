6ca83fd4ae613ed11f22eb082873083484fb697f
Jaum09 <joao.silva62@estudante.ifms.edu.br>
2023-10-31 06:46:42 -0400

    Construção da classe banco

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,9 @@ app.get('/filmes',(req,res)=>{
 
 app.get('/filmes/:id',(req,res)=>{
     const id = parseInt(req.params.id)
-    const filme = filmesCadastros.find(filme=>filme.id === id)
-    if(!filme) return res.status(404).send('Filme não encontrado')
+    //FIND para buscar um filme pelo id e retornar para o usuário
+    const filme = filmesCadastros.find(filme => filme.id === id)
+    if(!filme) return res.status(404).send("Filme não encontrado")
     res.status(200).send(filme)
 })
 
